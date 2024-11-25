ed354d8a65a4b179731475d81f058e4d2d62e205
fajarajiprayoga <fajaraji.prayoga@gmail.com>
2024-11-25 16:01:41 +0700

    Debug

--- app/Filament/Pages/AssessorUserControllingDetail.php
+++ app/Filament/Pages/AssessorUserControllingDetail.php
@@ -110,26 +110,26 @@ class AssessorUserControllingDetail extends Page implements HasTable
                 ])
             ])
             ->filters([
-                    Filter::make('employee_name')
-                        ->form([
-                            TextInput::make('name')
-                        ])
-                        ->query(function (Builder $query, array $data) {
-                            if($data['name'] != ""){
-                                return $query->where('name', 'like', '%'.$data['name'].'%');
-                            }
-                        }),
-                    SelectFilter::make('section')
-                        ->relationship('section', 'name')
-                        ->multiple()
-                        ->searchable()
-                        ->preload(),
-                    SelectFilter::make('departement')
-                        ->relationship('section.departement', 'name')
-                        ->multiple()
-                        ->searchable()
-                        ->preload(),
-                    SelectFilter::make('position')->options(Position::get()->pluck('name', 'name'))->multiple()
+                    // Filter::make('employee_name')
+                    //     ->form([
+                    //         TextInput::make('name')
+                    //     ])
+                    //     ->query(function (Builder $query, array $data) {
+                    //         if($data['name'] != ""){
+                    //             return $query->where('name', 'like', '%'.$data['name'].'%');
+                    //         }
+                    //     }),
+                    // SelectFilter::make('section')
+                    //     ->relationship('section', 'name')
+                    //     ->multiple()
+                    //     ->searchable()
+                    //     ->preload(),
+                    // SelectFilter::make('departement')
+                    //     ->relationship('section.departement', 'name')
+                    //     ->multiple()
+                    //     ->searchable()
+                    //     ->preload(),
+                    // SelectFilter::make('position')->options(Position::get()->pluck('name', 'name'))->multiple()
                 ], layout: FiltersLayout::AboveContent)
                 ->actions([
                 ], position: ActionsPosition::BeforeCells)
